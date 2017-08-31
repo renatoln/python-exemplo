930e7c66a1ddd3509808c52fa9b16c13771e6523
“Cyndi <renatoln@yahoo.com.br>
2017-08-31 16:03:52 -0300

    primeiro exemplo Django, fist commit

---
+++ python_intro.py
@@ -0,0 +1,20 @@
+def ola(nome):
+	print('Ola '+ nome)
+
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+if 5 > 2 and False: #True
+	print ("maior")
+	for i in range(1, 6):
+		print(i)
+
+else:
+	print("menor")
+	for n in girls:
+		ola(n)
+
+
+# iniciar a virtualevn anaconda 
+# > source activate renatoenv
+# parar
+# > source deactivate
